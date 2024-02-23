2bb0f90af5dcd86b31a02c04c35a3f30f517f9ad
Awesh <aweshvishwakarma@gmail.com>
2024-02-23 16:42:08 +0530

    Added Basic Version Music Tagger

---
+++ SGSMTube-Lib/Models/MusicTagEditor.cs
@@ -0,0 +1,32 @@
+﻿using SGSMTube_Lib.Views;
+using System.Diagnostics;
+
+namespace SGSMTube_Lib.Models
+{
+    public class MusicTagEditor
+    {
+        public async Task UpdateMusicTag(string musicFilePath, MusicTagModel tagModel)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    var tFile = TagLib.File.Create(musicFilePath);
+                    if (tFile == null)
+                        return;
+                    tFile.Tag.Album = tagModel.Album;
+                    tFile.Tag.AlbumArtists = [tagModel.Artist];
+                    if (!string.IsNullOrEmpty(tagModel.Picture))
+                        tFile.Tag.Pictures = [new TagLib.Picture(tagModel.Picture)];
+                    tFile.Save();
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex);
+                }
+            });
+        }
+    }
+
+
+}
---
+++ SGSMTube-Lib/Views/MusicTagModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGSMTube_Lib.Views
+{
+    public class MusicTagModel
+    {
+        public string Title { get; set; }
+        public string Album { get; set; }
+        public string Artist { get; set; }
+        public string Picture { get; set; }
+    }
+}
